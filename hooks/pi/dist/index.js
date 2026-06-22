12dc16b7802f7d9da4ec5cec8bf5b69384182c7b
Alexander Pape <300609+Macavity@users.noreply.github.com>
2026-06-22 12:33:37 +0200

    feat(pi): TypeScript build pipeline (tsconfig + package.json + dist)

---
+++ hooks/pi/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = superpowersOptimizedPi;
+function superpowersOptimizedPi(_api) {
+    // Lifecycle subscribers wired in by Tasks 4–8.
+}
---
+++ hooks/pi/index.ts
@@ -0,0 +1,3 @@
+export default function superpowersOptimizedPi(_api: unknown): void {
+  // Lifecycle subscribers wired in by Tasks 4–8.
+}
